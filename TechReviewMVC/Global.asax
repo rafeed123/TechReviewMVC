a5322096e841006c131409ef3316094a66b488de
Rafee Daanish <rafee.daanish@gmail.com>
2017-02-10 11:31:35 -0500

    Added Models

---
+++ TechReviewMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TechReviewMVC.MvcApplication" Language="C#" %>
---
+++ TechReviewMVC/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechReviewMVC.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get;  set;}
+        public string Device { get; set ; }
+        public string Description { get; set; }
+        public string FullDescription { get; set; }
+        public int Price { get; set; }
+        public DateTime ManufactureDate { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
