c43f98811a8cf466fa647f090910af7d1a962621
Rafee Daanish <rafee.daanish@gmail.com>
2017-02-10 13:19:22 -0500

    added Reviews Controller

--- TechReviewMVC/Models/Category.cs
+++ TechReviewMVC/Models/Category.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace TechReviewMVC.Models
 {
     public class Category
     {
+        [Key]
         public int ID { get; set; }
         public string Manufacturer { get; set; }
         public virtual ICollection<Review> Reviews { get; set; }
